69e6140f400adab251f7e1643aac0168700c40d9
Siri227 <94301487+Siri227@users.noreply.github.com>
2021-11-16 09:57:10 +0530

    Add files via upload

--- FBG.m
+++ FBG.m
@@ -1,94 +1,101 @@
 %clear;
 % r -> reflection spectrum of FBG
 % tau -> time delay of FBG
-% lamda -> wavelength on which calculation has been made
-function [r,tau,lamda] = FBG(varargin)
+% w -> wavelength on which calculation has been made
+%input arguements:  Lg                  length of FBG in cm
+%                   neff                effective group RI at 1550nm
+%                   del_neff            index change of RI
+%                   Chirp_var           d(lamda)/dz in nm/cm
+%                   epodize_fxn_choice  choice of epodization function
+%                                       1 - uniform
+%                                       2 - gaussian
+%                                       3 - raised cosine
+function [r,tau,w] = FBG(varargin)
 %% input grating parameter
 % setting up default values
 if length(varargin)>5
     error('Too Many Arguements in FBG function')
 end
 
-defaults = {2,0.04,1.447,10*1e-9,100};
+defaults = {0.5,1.4682,0.0004,-2.5,1};
 defaults(1:nargin) = varargin;
 
-[choice,L,n_eff,C,N] = defaults{:};
+[Lg,neff,del_neff,chirp_var,epodize_fxn_choice] = defaults{:};
 
+%% calculation of chirp
+%operating wavelength in nm
+wd = 1550;
+%wavelength precision for optical spectrum calculation in nm
+del_w = 0.01;
+%segmentation of FBG
+N = 500;
+%length array 
+z = linspace(0,Lg,N);
+dz = z(2) - z(1);
+%chirp -> rate of phase change along z direction in FBG
+chirp = (-4*pi*neff*(chirp_var*1e-7)/((wd*1e-9)^2))*(z*1e-2);
 
-delta_neff = 0.00005;    % index change
-bragg_period = 1e-4;    % bragg period
-del_n = 0.0004;         % depth of modulation
-lambdad = 1.55*1e-6;     % design grating wavelength
+%% apodization function
+switch epodize_fxn_choice
+    case 1
+        gz = ones(1,N); %uniform chirp
+    case 2
+        gz = exp(-64*((z - Lg/2)/Lg).^4); %gaussian profile
+    case 3
+        gz = 0.5*(1 + cos(pi*(z - Lg/2)/Lg)); %raised cosine 
+    otherwise
+        error('invalid choice value')
+end
 
-bragg_p = lambdad/(2*n_eff);   % bragg period 
-
-%% refractive index variation along z
-syms x y; % x as z bcs of fcontour
-% fprintf('choose apodizatoin function:\n')
-% fprintf('1: uniform grating \n2: gaussian profile \n3: raused cosine \n');
-% choice = input('choose from above(frm 1 to 3): ');
-% while ((choice > 3) || (choice < 1))
-%     choice = input('invalid choice\nenter again: ');
-% end
-%apodization function
-gz1 = 1;                     % uniform grating
-
-a = 64;                      % gaussian profile
-gz2 = exp(-a*((x - L/2)/L).^4);
- 
-gz3 = 0.5*(1 + cos(pi*(x - L/2)/L)); % raised cosine
-gzs = [gz1 gz2 gz3];
-
-gz = gzs(choice);
-
-%% steps for simulatoin
-%N;              %Segmentation of FBG
-M=1501;            %Total points of computation
-
-%% adopization function
-ap_fn = double(subs(gz,linspace(0,L,N)));
-delta_n_eff = delta_neff*ap_fn;
-
-%% input wavelength
-lamda1=lambdad*1e9 - 1;
-lamda2=lambdad*1e9 + 1;
-lamda=linspace(lamda1,lamda2,M)*1e-9; %Wavelength span
-delta_lamda=(lamda2-lamda1)/M*1e-9;
+%% wavelength span for FBG
+% 2nm wavelenght span 
+%total number of points for calculation
+M = round(10/del_w);
+%input wavelength
+w = linspace(wd-5,wd+5,M)*1e-9;
+%bragg wavelengths variation along z direction
+wb = linspace(wd - chirp_var*Lg/2,wd + chirp_var*Lg/2,N)*1e-9;
 
 %% initializing reflectence, phase, delay vector
-r=zeros(1,length(lamda));
-phi=zeros(1,length(lamda));
-tau=zeros(1,length(lamda));
+r=zeros(1,length(w));
+phi=zeros(1,length(w));
 
 %% transmisson matrix method
-for k=1:M
-    F=[1,0;0,1];  %Initialize transmission matrix
-    for i=1:N
-        %£¨1£©Uniform
-        %delta_n_eff=0.00005;
-        %delta_n_eff=0.00005*exp((-64*(-L/2+i*L/N)^4)/L^4);
-        lamda_D=(1550 + C*L*(i/N - 0.5))*1e-9;  %The center wavelength of each fiber grating segment
-        sigma=2*pi*n_eff*(1/lamda(k)-1/lamda_D)+2*pi*delta_n_eff(i)/lamda(k)+(4*pi*n_eff)*C*(-L/2+i*L/N)/lamda_D^2;  %Self coupling coefficient
-        kappa=pi*delta_n_eff(i)/lamda(k);  %AC coupling coefficient
-        omega=sqrt(kappa^2-sigma^2);
-        
-        f11=cosh(omega*L/N)-1i*(sigma/omega)*sinh(omega*L/N);
-        f12=1i*(kappa/omega)*sinh(omega*L/N);
-        f21=-1i*(kappa/omega)*sinh(omega*L/N);
-        f22=cosh(omega*L/N)+1i*(sigma/omega)*sinh(omega*L/N);
-        F=F*[f11,f12;f21,f22];  %Transmission matrix
+dz = dz*1e-2;
+z = z*1e-2;
+for i = 1:M
+    %DC coupling coefficient
+    sigmaz = (2*pi*neff)*(1./w(i) - 1./wb) + (2*pi*del_neff)./w(i) - chirp;
+    %AC coupling coefficinet
+    kz = (pi*del_neff)*(gz/w(i));
+    %gamma b
+    gamma = sqrt(kz.^2 - sigmaz.^2);
+    %matrix elements
+    f11 = cosh(gamma*dz) - 1j*(sigmaz./gamma).*sinh(gamma*dz);
+    f12 = -1j*(kz./gamma).*sinh(gamma*dz);
+    f21 = 1j*(kz./gamma).*sinh(gamma*dz);
+    f22 = cosh(gamma*dz) + 1j*(sigmaz./gamma).*sinh(gamma*dz);
+    
+    %initializing transmission matrix
+    F = [1,0;0,1];
+    %calculation of transmission matrix for entire FBG for w(i)
+    for j = 1:N
+        F = F*[f11(j), f12(j);f21(j), f22(j)];
     end
-    r(k)=(abs(F(3)/F(1)))^2;  %Magnitude of refractive rate
-    phi(k)=phase(F(3)/F(1));  %Phase of refractive rate
+    %reflective rate for wavelength w(i)
+    r(i) = (abs(F(3)/F(1))).^2;
+    %phase of w(i)
+    phi(i) = angle(F(3)/F(1));
 end
 
 dtg = [phi(1) diff(phi)];
 s = std(dtg);
-plot(diff(dtg))
+% plot(diff(dtg))
 dtg(abs([0 diff(dtg)])>s) = nan;
 
-tau = -((lamda.^2)/(2*pi*3e8)).*(dtg/delta_lamda)*1e12;
-
+tau = -((w.^2)/(2*pi*3e8)).*(dtg/del_w)*1e12;
+%conerting back to nm
+w = w*1e9;
 
 end %function end
 
